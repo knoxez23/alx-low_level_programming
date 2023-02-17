253a9a23b38257e592bee905225b1acab39493d3
Lennox Kabo <borelennox@gmail.com>
2023-02-17 11:05:25 -0800

    n/p corr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,27 @@
 #!/bin/bash
-if(n>0) {
-	printf("is positive")
-}
-if(n=0){
-	printf("is zero")
-}
-if(n<0){
-	printf("is negative")
-}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n>0){
+		printf("%d",n);
+		pritnf("is positive");
+	}
+	else if(n==0){
+		printf("%d",n);
+		printf("is zero");
+	}
+	else if(n<0){
+		printf("%d",n);
+		printf("is negative");
+	}
+	return (0);
+
