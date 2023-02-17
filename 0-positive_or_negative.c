bf0f617bd1cf1d633da1c69ea82431e0f4093343
Lennox Kabo <borelennox@gmail.com>
2023-02-17 10:38:15 -0800

    negative&positive n.os

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+if(n>0){
+	printf("%d",n);
+	printf("is positive");}
+if(n=0){
+	printf("%d",n);
+	printf("is zero");
+}
+if(n<0){
+	printf("%d",n);
+	printf("is negative");
+}
---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+if(n>0) {
+	printf("is positive")
+}
+if(n=0){
+	printf("is zero")
+}
+if(n<0){
+	printf("is negative")
+}
