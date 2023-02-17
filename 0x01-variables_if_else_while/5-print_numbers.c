706f347120fac7e80462ceab98803ceb2c239c3e
Lennox Kabo <borelennox@gmail.com>
2023-02-17 12:50:01 -0800

    print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return:0 (Successful)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10) {
+			printf("%d", x);
+			x++;
+		}
+	printf("\n");
+	return (0);
+
+}
