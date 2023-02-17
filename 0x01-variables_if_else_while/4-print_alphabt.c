ea6aeffa2b3ada52f350b41fe13ebf1004ff9b32
Lennox Kabo <borelennox@gmail.com>
2023-02-17 12:41:23 -0800

    print alphabets with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0(Successful)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while
+		(x <= 'z') {
+			if ((x != 'e' && x != 'q') && x <= 'z')
+			putchar(x);
+	x++;
+		}
+	putchar('\n');
+	return (0);
+
+
+}
