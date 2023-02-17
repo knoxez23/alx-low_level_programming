74e2cca649d0f08c7aae57f18b302a3c1897ce51
Lennox Kabo <borelennox@gmail.com>
2023-02-17 13:19:35 -0800

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0(Successful)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
