a5691a7882554dc38259eb6ede939704f06dee3d
Lennox Kabo <borelennox@gmail.com>
2023-02-17 12:28:31 -0800

    lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0(Successful)
+ */
+int main(void)
+{
+	char x;
+	char y;
+
+	x = 'a';
+	y = 'A';
+
+	while
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	while
+		(y <= 'Z') {
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+
+}
