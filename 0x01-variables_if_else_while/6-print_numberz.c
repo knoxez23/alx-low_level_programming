86352d00b9566688bde302859b6d644c40331d79
Lennox Kabo <borelennox@gmail.com>
2023-02-17 12:55:05 -0800

    single digits with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0(successful)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10) {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+
+}
