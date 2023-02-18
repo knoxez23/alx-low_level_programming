d57d119c93a1e02bacf565286b897eba2697434b
Lennox Kabo <borelennox@gmail.com>
2023-02-18 09:09:43 -0800

    print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: o(Successful)
+ */
+int main(void)
+{
+	char print[] = "Programming is like building a multilingual puzzle";
+	puts(print);
+	return (0);
+}
