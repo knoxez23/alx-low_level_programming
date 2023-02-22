d184b204247969c2f7d2708473509858e124c85c
Lennox Kabo <borelennox@gmail.com>
2023-02-21 18:29:06 -0800

    ..

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character a to stdout
+ * a: character to print
+ *
+ * Return: 0(Success)
+ */
+int _putchar(char a)
+{
+	return (write(1, &c, 1));
+}
