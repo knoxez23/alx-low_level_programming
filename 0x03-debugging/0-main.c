abb865b4853d2991f1bc2200662629c67ecd905c
Lennox Kabo <borelennox@gmail.com>
2023-02-23 13:35:46 -0800

    ,,

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,12 @@
 #include "main.h"
-
 /**
-<<<<<<< HEAD
- * main - Test function for positive or negative
- * Return: 0
- */
-
-=======
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- * 
- */
->>>>>>> 8218854b8abebadca6f13bc8eb2f5790cd3e9a5f
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-<<<<<<< HEAD
-
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
-=======
-	return (0);
->>>>>>> 8218854b8abebadca6f13bc8eb2f5790cd3e9a5f
 }
