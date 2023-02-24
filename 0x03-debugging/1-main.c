c0c38254167307bf388e9da902d10a467426f502
Lennox Kabo <borelennox@gmail.com>
2023-02-24 00:20:06 -0800

    crr

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
 int i;
@@ -16,6 +18,5 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
