1c1fc401319d6cbcd41f999614593c882df9cfcb
Lennox Kabo <borelennox@gmail.com>
2023-03-08 22:26:56 +0300

    ;;

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @a: The string to be printed.
  */
-void _print_rev_recursion(char *a);
+void _print_rev_recursion(char *a)
 {
 	if (*a)
 	{
