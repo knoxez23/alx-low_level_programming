dd5f21f66bd54b49c5faa26bfee5711dfe157556
Lennox Kabo <borelennox@gmail.com>
2023-07-19 01:28:16 +0300

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+//main: has return value of 0
 int main(void)
 {
 	_putchar('_');
@@ -10,6 +10,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 
