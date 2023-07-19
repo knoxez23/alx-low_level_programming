59562321d321ed5e3d014ae07ed733ce3f3a8734
Lennox Kabo <borelennox@gmail.com>
2023-07-20 02:36:19 +0300

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,39 +7,31 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else
-            {
-                if (result < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar(' ');
-                }
-
-                _putchar((result) + '0');
-            }
-
-            if (j != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = (i * j);
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (result < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}
+			else
+				_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
