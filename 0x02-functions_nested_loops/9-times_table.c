f01312a7b8b4f23ee4659dbcd0b19ee6953f2585
Lennox Kabo <borelennox@gmail.com>
2023-07-20 00:58:28 +0300

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,47 @@
 #include "main.h"
 
 /**
- * times_table - print the 9 times table, starting with 0
+ * times_table - prints the 9 times table, starting with 0
  *
  * Return: void
- *
  */
 void times_table(void)
 {
-	int i, j, result;
+    int i, j, result;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			result = i * j;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
 
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				if (result < 10)
-				{
-					_putchar(' ');
-				}
+            if (j == 0)
+            {
+                _putchar('0'); // Print '0' for the first column
+            }
+            else
+            {
+                // Print spaces before the number to align the table
+                if (result < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' '); // Two spaces for single-digit numbers
+                }
+                else if (result >= 10 && result < 100)
+                {
+                    _putchar(' '); // One space for two-digit numbers
+                }
 
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			
-			if (j != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+
+            if (j != 9)
+            {
+                _putchar(','); // Print comma for all columns except the last one
+                _putchar(' '); // Print space after the comma
+            }
+        }
+        _putchar('\n');
+    }
 }
