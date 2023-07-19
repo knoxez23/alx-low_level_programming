aea465103c0e760e650fb0f1fdc22005709d44f2
Lennox Kabo <borelennox@gmail.com>
2023-07-19 23:45:48 +0300

    federal agent timer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,9 +15,11 @@ void jack_bauer(void)
 		while (min < 60)
 		{
 			min++;
-			_putchar(h);
-			_putchar(':');
-			_putchar(min);
+			_putchar((h / 10) + '0'); //Print the tens digit of the hour
+			_putchar((h % 10) + '0'); //Print the ones digit of the hour
+			_putchar(:)
+			_putchar((min / 10) + '0'); //Print the tens digit of the hour
+			_putchar((min % 10) + '0'); //Print the ones digit of the hour
 			_putchar('\n');
 		}
 	}
