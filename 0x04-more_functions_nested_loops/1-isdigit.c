fc8a94011916e1970b793ead2f9f4490e93fd232
Lennox Kabo <borelennox@gmail.com>
2023-07-21 22:57:34 +0300

    0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if number falls from 0 to 0
+ * @c: number being checked
+ *
+ * Return: 1 if number falls in category, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <=57)
+	{
+		return (1);
+	}
+	return (0);
+}
