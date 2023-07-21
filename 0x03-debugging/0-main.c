ad4850b5b5ea8bd77bbc9f15b0253ac44aeef111
Lennox Kabo <borelennox@gmail.com>
2023-07-21 19:19:28 +0300

    testing function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function for testing positive_or_negative
+ *
+ * Return: 0
+ */
+int main()
+{
+	int i = 0;
+
+	void positive_or_negative(i);
+
+	return (0);
+}
