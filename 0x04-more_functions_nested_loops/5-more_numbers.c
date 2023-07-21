46a40ba970846890539c7fa38b90995a4f233b06
Lennox Kabo <borelennox@gmail.com>
2023-07-21 23:55:09 +0300

    print 10 times [0-14]

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times [0-14]
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+
+		_putchar('\n');
+	}
+}
