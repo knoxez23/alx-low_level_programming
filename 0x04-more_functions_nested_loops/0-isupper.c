5711db33611df80aaba9464ceaee45b360c897a0
Lennox Kabo <borelennox@gmail.com>
2023-07-21 22:39:43 +0300

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_upper - function checks for uppercase character
+ *
+ * Return: 1 for uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c == "A-Z")
+	{
+		return (1);
+	}
+
+	return (0);
+}
