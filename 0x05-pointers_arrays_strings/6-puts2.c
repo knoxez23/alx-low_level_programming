b17f31e0c434dd071b88eff6c6982d6973608f4a
Lennox Kabo <borelennox@gmail.com>
2023-07-24 18:53:46 +0300

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "mqin.h"
+
+/**
+ * puts2 - print every string character
+ * @str
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
