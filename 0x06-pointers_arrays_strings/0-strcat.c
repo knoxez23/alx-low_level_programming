8a91cb9268aaca6ec78b36797b4f80625b91a13c
Lennox Kabo <borelennox@gmail.com>
2023-07-28 00:27:35 +0300

    concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#inlcude <stdio.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer
+ * @src: pointer
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	char* dest_ptr = dest;
+
+	while (*dest_ptr != '\0')
+	{
+		dest_ptr++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++
+	}
+
+	*dest_ptr = '\0';
+
+	return dest;
+}
