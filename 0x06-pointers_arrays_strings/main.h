7f8978b6111bf5a34d93ce231235a48a45cc2be2
Lennox Kabo <borelennox@gmail.com>
2023-07-28 13:53:56 +0300

    advanced tasks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *rot13(char *s);
+void print_number(int n);
+void rev_string(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
