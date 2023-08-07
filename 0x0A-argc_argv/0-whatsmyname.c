fa1b26a8442f1ac526d9c0faed392b7ff83aa168
Lennox Kabo <borelennox@gmail.com>
2023-08-07 19:28:37 +0300

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s", argv[0]);
