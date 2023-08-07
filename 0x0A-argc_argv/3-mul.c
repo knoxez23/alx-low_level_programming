a2d7a19520b55cb2018b02b8bb676c3515d2a6d3
Lennox Kabo <borelennox@gmail.com>
2023-08-07 21:05:27 +0300

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,18 +13,18 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc = 3)
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
 	{
 		int num1 = atoi(argv[1]);
 		int num2 = atoi(argv[2]);
 
 		result = num1 * num2;
 		printf("%d", result);
-	}
-	else
-	{
-		printf("Error");
-		return (1);
 	}
 
 	return (0);
