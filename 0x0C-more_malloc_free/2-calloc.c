940290b605f048ee26a939d130f189db94597722
Lennox Kabo <borelennox@gmail.com>
2023-08-11 00:40:06 +0300

    function that allocates array mem

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -29,7 +29,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	if (nmemb == 0 || size == 0)
+	if (!(nmemb && size))
 	{
 		return (NULL);
 	}
