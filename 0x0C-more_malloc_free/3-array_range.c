291b76ade1b136997116ce5df25860df0a3e687b
Lennox Kabo <borelennox@gmail.com>
2023-08-11 01:08:31 +0300

    function that creates array of strings

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,19 @@
  */
 int *array_range(int min, int max)
 {
-	if (min > max)
+	int *arr, i;
+	unsigned int num_elements;
+
+	if (max < min)
 	{
 		return (NULL);
 	}
 
-	int num_elements = max - min + 1;
-	int *arr = (int *)malloc(num_elements * sizeof(int));
+	num_elements = max - min + 1;
+	arr = (int *)malloc(num_elements * sizeof(int));
 
-	if (arr == NULL)
+	if (!arr)
 		return (NULL);
-	int i;
 
 	for (i = 0; i < num_elements; ++i)
 	{
