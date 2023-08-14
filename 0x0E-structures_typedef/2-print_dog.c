e777a5572eb6bcbe22b7bc9c6334815b8415d726
Lennox Kabo <borelennox@gmail.com>
2023-08-15 02:19:19 +0300

    print dog members

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name) != NULL ? d->name : "(nil)");
-		printf("Age: %f\n", (d->age) >= 0 ? d->age : 0);
-		printf("Owner: %s\n", (d->owner) != NULL ? d->owner : "(nill)");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nill)");
 	}
 }
