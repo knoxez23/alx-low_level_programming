012501c407499d74f55121ae3c0cd743cf995d09
Lennox Kabo <borelennox@gmail.com>
2023-08-17 01:12:08 +0300

    function that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_name - function that prints name
+ * @name: variable pointer
+ * @f: function pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
