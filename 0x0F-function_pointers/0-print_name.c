63717577a7e85befd785fbd60f01bb191955ac48
Lennox Kabo <borelennox@gmail.com>
2023-08-17 17:39:32 +0300

    correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - function that prints a name
@@ -12,47 +11,4 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 		f(name);
-}
-void print_name_as_is(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
 }
