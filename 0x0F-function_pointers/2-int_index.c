236b3374d95b341c5dd1c8151a2c03e8f63badbc
Lennox Kabo <borelennox@gmail.com>
2023-08-17 18:37:15 +0300

    correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,17 +11,18 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (size > 0)
 	{
 		if (array != NULL  && cmp != NULL)
 		{
-			int i = 0;
 			
-			while (i < size)
+			for (i = 0; i < size; i++)
 			{
 				if (cmp(array[i]))
 					return (i);
-				i++
+				return (-1);
 			}
 		}
 	}
