053f7ce927ded6b49c28e4c097ff354093fe6fbe
Lennox Kabo <borelennox@gmail.com>
2023-08-18 02:42:16 +0300

    task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,10 +10,11 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
 
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
 		if (i < n - 1 && separator != NULL)
